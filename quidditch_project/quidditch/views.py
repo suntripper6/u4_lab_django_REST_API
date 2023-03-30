e7dc6e29772e49f8a4cab7b427e1c156ff896201
Jason Bundy <jason.bundy@me.com>
2023-03-30 14:22:03 -0400

    initial commit lab completed

---
+++ quidditch_project/quidditch/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Team, Player
+from rest_framework import generics
+from .serializers import TeamSerializer, PlayerSerializer
+from .models import Team, Player
+
+# Create your views here.
+
+
+class TeamList(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
