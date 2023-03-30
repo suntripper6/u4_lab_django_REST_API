20642f225576a56186e348ce16a447cfad60bf9b
Jason Bundy <jason.bundy@me.com>
2023-03-30 15:26:03 -0400

    tweaks

--- quidditch_project/quidditch/serializers.py
+++ quidditch_project/quidditch/serializers.py
@@ -2,6 +2,23 @@ from rest_framework import serializers
 from .models import Team, Player
 
 
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    team = serializers.HyperlinkedRelatedField(
+        view_name='team_detail',
+        read_only=True
+    )
+
+    team_id = serializers.PrimaryKeyRelatedField(
+        queryset=Team.objects.all(),
+        source='team'
+    )
+
+    class Meta:
+        model = Player
+        fields = ('id', 'name', 'position', 'age',
+                  'injured_reserve_list', 'team', 'team_id')
+
+
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     players = serializers.HyperlinkedRelatedField(
         view_name='player_detail',
@@ -9,19 +26,11 @@ class TeamSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True
     )
 
-    class Meta:
-        model = Team
-        fields = ('id', 'name', 'location', 'division',
-                  'wins', 'losses', 'players')
-
-
-class PlayerSerializer(serializers.HyperlinkedModelSerializer):
-    team = serializers.HyperlinkedRelatedField(
-        view_name='team_detail',
-        read_only=True
+    team_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='team_detail'
     )
 
     class Meta:
-        model = Player
-        fields = ('id', 'name', 'position', 'age',
-                  'injured_reserve_list', 'team')
+        model = Team
+        fields = ('id', 'team_url', 'name', 'location', 'division',
+                  'wins', 'losses', 'players')
